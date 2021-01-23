8aaf1dbd507114753b625df7aa5510e5c98ed787
CheungLiu <CheungLiu@163.com>
2021-01-23 20:27:10 +0800

    234回文链表

---
+++ LeetCode/234_1.cpp
@@ -0,0 +1,92 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+    ListNode *frontPointer;
+
+public:
+    bool recursivelyCheck(ListNode *currentNode)
+    {
+        if (currentNode != nullptr)
+        {
+            if (!recursivelyCheck(currentNode->next))
+            {
+                return false;
+            }
+            if (currentNode->val != frontPointer->val)
+            {
+                return false;
+            }
+            frontPointer = frontPointer->next;
+        }
+        return true;
+    }
+
+    bool isPalindrome(ListNode *head)
+    {
+        frontPointer = head;
+        return recursivelyCheck(head);
+    }
+};
+template <typename T>
+class print
+{
+public:
+    void operator()(const T &elem)
+    {
+        cout << elem << ' ';
+    }
+};
+
+int main(int argc, char **argv)
+{
+    cout << "Test!" << endl;
+    int ia[] = {1, 2, 3, 4, 5};
+    vector<int> iv(ia, ia + 5);
+    for_each(iv.begin(), iv.end(), print<int>());
+
+    Solution obj = Solution();
+    ListNode *head = new ListNode(-1);
+    ListNode *p = head, *tmp = nullptr;
+    for (vector<int>::const_iterator iter = iv.begin(); iter != iv.end(); iter++)
+    {
+        tmp = new ListNode(*iter);
+        head->next = tmp;
+        head = head->next;
+    }
+
+    head = p->next;
+    // head = obj.reverseKGroup(head, 1);
+    p = head;
+    while (head)
+    {
+        cout << head->val;
+        head = head->next;
+    }
+    delete p;
+    delete head;
+    return 0;
+}
